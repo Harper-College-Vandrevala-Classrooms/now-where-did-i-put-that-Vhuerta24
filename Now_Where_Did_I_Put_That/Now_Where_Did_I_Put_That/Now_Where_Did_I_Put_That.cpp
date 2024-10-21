5a9991a90a2c76908b73df67ce485f73f7e60818
Vhuerta24 <vanessaht691@gmail.com>
2024-10-20 21:02:05 -0500

    case sensitivity and starting position

--- Now_Where_Did_I_Put_That/Now_Where_Did_I_Put_That/Now_Where_Did_I_Put_That.cpp
+++ Now_Where_Did_I_Put_That/Now_Where_Did_I_Put_That/Now_Where_Did_I_Put_That.cpp
@@ -1,35 +1,52 @@
-//In this lab we have to create 2 functions, one that finds a specifc character in a given string and another that finds a substring in a given string. 
-//The output should be the index of the where the character/substring is found or return -1 to show not found
 #include <iostream>
 #include <string>
+#include <cctype> // for tolower
 
-using namespace std; 
+using namespace std;
 
-//character 
-int findThe(const string& str, char _char) {
-    for (int i = 0; i < str.length(); ++i) {
-        if (str[i] == _char) {
+// Character search with case sensitivity and starting position options
+int findThe(const string& str, char _char, bool caseSensitive = true, int startPos = 0) {
+    if (startPos < 0 || startPos >= str.length()) {
+        return -1; // Invalid starting position
+    }
+
+    for (int i = startPos; i < str.length(); ++i) {
+        char currentChar = str[i];
+        if (!caseSensitive) {
+            currentChar = tolower(currentChar);
+            _char = tolower(_char);
+        }
+        if (currentChar == _char) {
             return i; // Return the index of the first occurrence
         }
     }
     return -1; // Returns -1 if the character is not found
 }
 
-//substring 
-int findThe(const string& str, const string& substr) {
+// Substring search with case sensitivity and starting position options
+int findThe(const string& str, const string& substr, bool caseSensitive = true, int startPos = 0) {
+    if (startPos < 0 || startPos >= str.length()) {
+        return -1; // Invalid starting position
+    }
+
     int strLen = str.length();
     int substrLen = substr.length();
 
-    //Loops through the main string
-    for (int i = 0; i <= strLen - substrLen; ++i) {
+    for (int i = startPos; i <= strLen - substrLen; ++i) {
         int j;
-        // Checks if the substring matches
         for (j = 0; j < substrLen; ++j) {
-            if (str[i + j] != substr[j]) {
+            char currentChar = str[i + j];
+            char substrChar = substr[j];
+
+            if (!caseSensitive) {
+                currentChar = tolower(currentChar);
+                substrChar = tolower(substrChar);
+            }
+
+            if (currentChar != substrChar) {
                 break;
             }
         }
-        // If the inner loop completed, we found a match
         if (j == substrLen) {
             return i; // Return the starting index of the substring
         }
@@ -37,84 +54,47 @@ int findThe(const string& str, const string& substr) {
     return -1; // Return -1 if the substring is not found
 }
 
+// Function to perform tests
+void performTest(const string& str, char c, const string& substr, bool caseSensitive = true, int startPos = 0) {
+    cout << "Testing on string: \"" << str << "\" from position " << startPos << endl;
 
+    int charIndex = findThe(str, c, caseSensitive, startPos);
+    if (charIndex != -1) {
+        cout << "Character '" << c << "' found at index: " << charIndex << endl;
+    }
+    else {
+        cout << "Character not found: -1" << endl;
+    }
+
+    int substrIndex = findThe(str, substr, caseSensitive, startPos);
+    if (substrIndex != -1) {
+        cout << "Substring \"" << substr << "\" found at index: " << substrIndex << endl;
+    }
+    else {
+        cout << "Substring not found: -1" << endl;
+    }
+
+    cout << endl; 
+}
 
 int main() {
+    // Test case-sensitive searches from different starting positions
+    performTest("Hello, world!", 'H', "H");
+    performTest("Hello, world!", 'h', "h"); // Should not find 'h' case-sensitity (upper/lower)
+    performTest("Hello, world!", ' ', "rld");
 
-    //test 1 
+    // Test case-insensitive searches from different starting positions
+    performTest("Hello, world!", 'h', "h", false); // Case insensitive
+    performTest("Hello, world!", 'H', "H", false); // Case insensitive
+    performTest("Hello, world!", '!', "GOODBYE", false); // Should not find 'GOODBYE'
+    performTest("Hello, world!", '!', "World", false); // Should find 'World'
 
-    string myString = "Hello, world!";
-    char myChar = 'H';//index = 0
-    string mySubstr = "H";//index = 0
-    
-    int CharIndex = findThe(myString, myChar);
-    if (CharIndex != string::npos) {
-        cout << "TEST 1" << endl;
-        cout << "Character '" << myChar << "' found at index: " << CharIndex << endl;// Outputs where(index) the character is
-    } 
-    else {
-        cout << "Character not found: -1" << endl; //If the character isnt found a -1 will be outputted to show that. 
-    }
-
-    int StrIndex = findThe(myString, mySubstr);
-    if (StrIndex != -1) {
-        cout << "Substring " << mySubstr << " found at index: " << StrIndex << endl;// Outputs where(index) the substring begins 
-    }
-    else {
-        cout << "Substring not found: -1 " << endl;
-    }
-
-
-
-    //TEST 2 
-
-    string String2 = "Hello, world!";
-    char Char2 = ' ';// index = 6
-    string Substr2 = "rld";//index = 9
-
-    int Char2Index = findThe(String2, Char2);
-    if (Char2Index != string::npos) {
-        cout << "TEST 2" << endl;
-        cout << "Character '" << Char2 << "' found at index: " << Char2Index << endl;// Outputs where(index) the character is
-    }
-    else {
-        cout << "Character not found: -1" << endl; //If the character isnt found a -1 will be outputted to show that. 
-    }
-
-    int Str2Index = findThe(String2, Substr2);
-    if (Str2Index != -1) {
-        cout << "Substring " << Substr2 << " found at index: " << Str2Index << endl;// Outputs where(index) the substring begins 
-    }
-    else {
-        cout << "Substring not found: -1 " << endl;
-    }
-
-
-    //TEST 3 
-
-    string String3 = "Hello, world!";
-    char Char3 = '!';// index = 12
-    string Substr3 = "goodbye";//index = not found
-
-    int Char3Index = findThe(String3, Char3);
-    if (Char3Index != string::npos) {
-        cout << "TEST 3" << endl;
-        cout << "Character '" << Char3 << "' found at index: " << Char3Index << endl;// Outputs where(index) the character is
-    }
-    else {
-        cout << "Character not found: -1" << endl; //If the character isnt found a -1 will be outputted to show that. 
-    }
-
-    int Str3Index = findThe(String3, Substr3);
-    if (Str3Index != -1) {
-        cout << "Substring " << Substr3 << " found at index: " << Str3Index << endl;// Outputs where(index) the substring begins 
-    }
-    else {
-        cout << "Substring not found: -1 " << endl;
-    }
-
+    // Test with starting position
+    performTest("Hello, world!", 'o', "rld", true, 5); // Start from index 5
+    performTest("Hello, world!", 'o', "ld", true, 3); // Start from index 3
+    performTest("Hello, world!", '!', "d", true, 9); // Start from index 5
+    performTest("Hello, world!", ' ', " ", true, 2); // Start from index 5
+    performTest("Hello, world!", 'o', "rld", false, 5); // Case insensitive, start from index 5
 
     return 0;
 }
-
-
